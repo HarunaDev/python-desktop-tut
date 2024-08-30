c4b6efa377b78ee29ff0aa7a93b10ca82ea1fd16
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-30 20:42:05 +0100

    added guide to building app
    starting by building a simple application to practice layout and handling events

---
+++ app.py
@@ -0,0 +1,11 @@
+# All imports
+
+# Main app objects and settings
+
+# Create all widgets needed in app
+
+# Design your layout, add your widgets to the screen
+
+# Set the final layout to the main window & events
+
+# Show and execute your app
