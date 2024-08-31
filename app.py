0b5b61dc36c40095d9c5f989829496d7d43c1e56
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-31 03:46:31 +0100

    created application with widget and title

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 # All imports
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
 
 # Main app objects and settings
+app = QApplication([])
+main_window = QWidget()
+main_window.setWindowTitle("My First App with PyQt5 yaaaay!")
+main_window.resize(200, 300)
 
 # Create all widgets needed in app
 
@@ -9,3 +15,5 @@
 # Set the final layout to the main window & events
 
 # Show and execute your app
+main_window.show()
+app.exec_()
