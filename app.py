66089e454b0a04481d0c7e8d6a85f5f39d172065
Alvin Haruna <harunaalvin69@gmail.com>
2024-08-31 04:02:27 +0100

    created more widgets for ui & layout boxes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # All imports
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout
 
 # Main app objects and settings
 app = QApplication([])
@@ -9,6 +9,20 @@ main_window.setWindowTitle("My First App with PyQt5 yaaaay!")
 main_window.resize(200, 300)
 
 # Create all widgets needed in app
+title_text = QLabel("Random Words")
+text1 = QLabel("Vietnam")
+text2 = QLabel("?")
+text3 = QLabel("goodbye")
+
+button1 = QPushButton("Click me")
+button2 = QPushButton("Click me")
+button3 = QPushButton("Click me")
+
+master_layout = QVBoxLayout()
+row1 = QHBoxLayout()
+row2 = QHBoxLayout()
+row3 = QHBoxLayout()
+
 
 # Design your layout, add your widgets to the screen
 
